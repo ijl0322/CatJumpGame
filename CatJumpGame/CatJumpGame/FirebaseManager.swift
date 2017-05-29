44fafb2600d8d7b498cf0b8700e42596d381c7a9
Isabel Lee <ijl0322@gmail.com>
2017-05-28 19:24:46 -0500

    saving JSON data downloaded from Firebase ok

--- CatJumpGame/CatJumpGame/FirebaseManager.swift
+++ CatJumpGame/CatJumpGame/FirebaseManager.swift
@@ -16,35 +16,22 @@ class FirebaseManager {
     let highScoreDataRef = FIRDatabase.database().reference(withPath: "highScores")
     let id = UIDevice().identifierForVendor?.uuidString
     
-    func test() {
-        print("posting")
-        let data: [String: Any] = ["tiles" : [ [12, 12, 13, 13, 13, 13, 12, 12],
-        [11, 11, 11, 11, 11, 11, 11, 11],
-        [8, 8, 8, 9, 9, 8, 8, 8],
-        [7, 7, 7, 7, 7, 7, 7, 7],
-        [2, 2, 2, 4, 4, 2, 2, 2],
-        [6, 6, 6, 6, 6, 6, 6, 6],
-        [5, 5, 5, 3, 3, 5, 5, 5],
-        [1, 0, 0, 0, 0, 0, 0, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0] ], "timeLimit": 50]
-        levelsRef.child("1").setValue(data)
+    func loadAllLevels() {
+        levelsRef.observeSingleEvent(of: .value, with: { snapshot in
+            //print(snapshot)
+            let levels = snapshot.value as! [[String: Any]]
+            dump(levels.count)
+            for (index, levelData) in levels.enumerated() {
+                self.saveToDoc(levelData: levelData, level: index + 1)
+            }
+        })
     }
-    
-//    func loadLevel(num: Int, completion: @escaping (([String:Any]) -> Void)) {
-//        levelsRef.child("\(num)").observeSingleEvent(of: .value, with: { snapshot in
-//            print(snapshot)
-//            let level = snapshot.value as! [String: Any]
-//            completion(level)
-//            self.saveToDoc(levelData: level)
-//            
-//        })
-//    }
     
     func loadLevel(num: Int) {
         levelsRef.child("\(num)").observeSingleEvent(of: .value, with: { snapshot in
             print(snapshot)
             let level = snapshot.value as! [String: Any]
-            self.saveToDoc(levelData: level)
+            self.saveToDoc(levelData: level, level: num)
         })
     }
     
@@ -86,14 +73,10 @@ class FirebaseManager {
         
     }
     
-    func saveToDoc(levelData: [String: Any]){
-        //let docs = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
-        //print("\(docs)")
-        //let url = docs.appending("/Level_1.json")
-        
+    func saveToDoc(levelData: [String: Any], level: Int){        
         guard let docs = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
         print(docs.absoluteString)
-        let fileUrl = docs.appendingPathComponent("Level1.json")
+        let fileUrl = docs.appendingPathComponent("Level_\(level).json")
         do {
             let data = try JSONSerialization.data(withJSONObject: levelData, options: [])
             try data.write(to: fileUrl, options: [])
